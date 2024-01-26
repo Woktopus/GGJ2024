7566bdd248269f24f60767d83f11e3551ca97709
Hadjin <augustemeyer86@gmail.com>
2024-01-27 00:34:06 +0100

    add weapon ui and ammo management
    gg easy

--- scripts/autoload/playersInfos.gd
+++ scripts/autoload/playersInfos.gd
@@ -1,17 +1,12 @@
 class_name playerInfos
 extends Node
 
-# player status infos
-const maxHealthPoint = 5
-const startHealthPoint = 3
-var healthPoint = 0
+var nbAmmo = 12
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	healthPoint = startHealthPoint
+	nbAmmo = 12
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
