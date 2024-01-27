124cbab5fd6588c2eaf823ecfa598ebb10b732e5
Briland Mathieu <briland.mathieu@gmail.com>
2024-01-27 14:25:55 +0100

    new camera + ennemy

--- scenes/gameplay/bullet/bullet.gd
+++ scenes/gameplay/bullet/bullet.gd
@@ -7,3 +7,12 @@ func _physics_process(delta):
 	var velocity = direction * speed * delta
 	global_position += velocity
 
+
+
+func _on_body_entered(body):
+	if "ennemy" in body.name:
+		queue_free()
+		body.kill()
+	else:
+		if body.name != "player":
+			queue_free()
---
+++ scenes/gameplay/ennemy/ennemy.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@export var player : CharacterBody2D
+
+func _ready():
+	player = get_parent().get_node("player")
+	$Timer.start()
+
+func _physics_process(delta):
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * SPEED
+
+	#position += (player.position - position) / 50
+	#look_at(player.global_position)
+	move_and_slide()
+	
+func makepath():
+	print(player.global_position)
+	nav_agent.target_position = player.global_position
+
+func kill():
+	queue_free()
+
+
+func _on_timer_timeout():
+	makepath()
