6ac4db0157a713c8a621a386aa28e10b423aa0e0
Hadjin <augustemeyer86@gmail.com>
2024-01-28 14:31:28 +0100

    add enemies healt management ad print blood on damage

--- scenes/gameplay/ennemy/ennemy.gd
+++ scenes/gameplay/ennemy/ennemy.gd
@@ -3,6 +3,7 @@ class_name Ennemy
 
 
 const SPEED = 75.0
+const MAX_HEALTH = 2
 
 
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
@@ -11,10 +12,13 @@ const SPEED = 75.0
 
 var lootScene = preload("res://scenes/gameProps/gunDrop.tscn")
 var deadBodyScene = preload("res://scenes/gameProps/deadBody.tscn")
+var bloodScene = preload("res://scenes/gameProps/blood.tscn")
+var healthPoint : int 
 
 func _ready():
 	player = get_parent().get_node("player")
 	$Timer.start()
+	healthPoint = MAX_HEALTH
 
 func _physics_process(_delta):
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
@@ -28,45 +32,53 @@ func makepath():
 	nav_agent.target_position = player.global_position
 
 func kill():
-	player.hasKill()
-	# spawn dead body on death 
 	var rng = RandomNumberGenerator.new()
 	var my_random_radius = rng.randf_range(0.0, 360.0)
-	var deadBody = deadBodyScene.instantiate()
-	deadBody.position = global_position
-	deadBody.set_rotation(my_random_radius)
-	#deadBody.
-	get_parent().add_child(deadBody)
-	print("print dead body")
-	
-	# chance to spawn ammo on death 
-	var my_random_number = rng.randf_range(0.0, 100.0)
-	if my_random_number >= 60.0 :
-		var loot  = lootScene.instantiate()
-		loot.position = global_position
-		loot.rotation_degrees = rotation_degrees
-		#loot.direction = Vector2.RIGHT.rotated(rotation)
-		get_parent().add_child(loot)
-		print("drop ammo!")
-	
-	# Add time to timer
-	
-	var funTimer = get_parent().get_node("FunTimer")
-	var timeLeft = funTimer.time_left
-	funTimer.stop()
-	print(timeLeft)
-	var newTime = timeLeft + 3.0
-	print(timeLeft)
-	if newTime > PlayersInfos.funTimerMaxValue :
-		newTime = PlayersInfos.funTimerMaxValue
-	print(timeLeft)
-	funTimer.start(newTime)
-	
-	# increase ennemies death counter
-	get_parent().increase_enemies_kill()
-	
-	# free current ennemie
-	queue_free()
+	healthPoint -= 1
+	if healthPoint > 0 :
+		var blood = bloodScene.instantiate()
+		blood.position = global_position
+		blood.set_rotation(my_random_radius)
+		get_parent().add_child(blood)
+		print("print blood")
+	else :
+		player.hasKill()
+		# spawn dead body on death 
+		var deadBody = deadBodyScene.instantiate()
+		deadBody.position = global_position
+		deadBody.set_rotation(my_random_radius)
+		#deadBody.
+		get_parent().add_child(deadBody)
+		print("print dead body")
+		
+		# chance to spawn ammo on death 
+		var my_random_number = rng.randf_range(0.0, 100.0)
+		if my_random_number >= 60.0 :
+			var loot  = lootScene.instantiate()
+			loot.position = global_position
+			loot.rotation_degrees = rotation_degrees
+			#loot.direction = Vector2.RIGHT.rotated(rotation)
+			get_parent().add_child(loot)
+			print("drop ammo!")
+		
+		# Add time to timer
+		
+		var funTimer = get_parent().get_node("FunTimer")
+		var timeLeft = funTimer.time_left
+		funTimer.stop()
+		print(timeLeft)
+		var newTime = timeLeft + 3.0
+		print(timeLeft)
+		if newTime > PlayersInfos.funTimerMaxValue :
+			newTime = PlayersInfos.funTimerMaxValue
+		print(timeLeft)
+		funTimer.start(newTime)
+		
+		# increase ennemies death counter
+		get_parent().increase_enemies_kill()
+		
+		# free current ennemie
+		queue_free()
 
 func _on_timer_timeout():
 	makepath()
